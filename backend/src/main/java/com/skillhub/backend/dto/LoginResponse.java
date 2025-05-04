019f7c67530182532d1bfc5b211f51c36175da90
Sheshan Perera <127820723+Sheshz@users.noreply.github.com>
2025-05-05 00:47:51 +0530

    Add LoginResponse.java

---
+++ backend/src/main/java/com/skillhub/backend/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.skillhub.backend.dto;
+
+import com.skillhub.backend.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private User user;
+}
